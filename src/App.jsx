35a275364bd2b5758f45c3f3cad153785e54ac93
wukang <893276941@qq.com>
2024-10-07 00:39:11 +0800

    feat: 更改路由

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Proposal />} />
-                {/* <Route path="/" element={<Home />} /> */}
+                <Route path="/home" element={<Home />} />
             </Routes>
         </BrowserRouter>
     );
